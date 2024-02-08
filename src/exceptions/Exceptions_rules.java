87de1a37a9a5b57ef6bd42ebb513eb32e34ded01
Albert Cordina <albert.cordina@dci-student.org>
2024-02-08 09:12:33 +0100

    update

--- src/exceptions/Exceptions_rules.java
+++ src/exceptions/Exceptions_rules.java
@@ -5,11 +5,11 @@ public class Exceptions_rules {
 	 * An exception is an event, which occurs during the execution of a program, that disrupts the normal flow of the program's instructions. 
 	 * When an error occurs within a method, the method creates an object and hands it off to the runtime system.
 	 * 
-	 * Unchecked exceptions − An unchecked exception is an exception that occurs at the time of execution. 
+	 * UNCHECKED exceptions − An unchecked exception is an exception that occurs at the time of execution. 
 	 * These are also called as Runtime-Exceptions. These include programming bugs, such as logic errors or improper use of an API. 
 	 * Runtime exceptions are ignored at the time of compilation.
 	 * 
-	 * Checked exceptions − A checked exception is an exception that is checked (notified) by the compiler at compilation-time, 
+	 * CHECKED exceptions − A checked exception is an exception that is checked (notified) by the compiler at compilation-time, 
 	 * these are also called as Compile time-Exceptions. These exceptions cannot simply be ignored, the programmer should take care of (handle) these exceptions.
 	 *  https://www.tutorialspoint.com/java/java_exceptions.htm
 	 *  
